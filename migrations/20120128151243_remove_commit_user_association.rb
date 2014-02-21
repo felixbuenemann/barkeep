2381afca6b8a9028c979694c3b9e610dd5b622e7
Felix Bünemann <buenemann@louis.info>
2014-02-21 15:05:02 +0100

    Fix migrations MySQL compatibility

--- migrations/20120128151243_remove_commit_user_association.rb
+++ migrations/20120128151243_remove_commit_user_association.rb
@@ -3,13 +3,16 @@ require "./" + File.join(File.dirname(__FILE__), "migration_helper")
 Sequel.migration do
   up do
     alter_table(:commits) do
+      drop_constraint :commits_ibfk_2, :type => :foreign_key
       drop_column :user_id
+      # when updatting squeel use:
+      # drop_foreign_key :user_id
     end
   end
 
   down do
     alter_table(:commits) do
-      add_foreign_key :user_id, :users
+      add_foreign_key :user_id, :users, :key => :id
     end
   end
 end
