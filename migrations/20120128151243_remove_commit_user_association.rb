ecfae087aa81678449f9cd53b09de678638584ee
Felix Bünemann <buenemann@louis.info>
2014-02-21 15:05:02 +0100

    Fix migrations MySQL compatibility

--- migrations/20120128151243_remove_commit_user_association.rb
+++ migrations/20120128151243_remove_commit_user_association.rb
@@ -3,13 +3,13 @@ require "./" + File.join(File.dirname(__FILE__), "migration_helper")
 Sequel.migration do
   up do
     alter_table(:commits) do
-      drop_column :user_id
+      remove_foreign_key :user_id
     end
   end
 
   down do
     alter_table(:commits) do
-      add_foreign_key :user_id, :users
+      add_foreign_key :user_id, :users, :key => :id
     end
   end
 end
